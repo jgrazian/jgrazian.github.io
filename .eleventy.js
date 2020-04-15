671166dfbdf7579c06f2899a6b1e08f69a7cb041
Joe Graziano <joeyg11@vt.edu>
2020-04-14 22:42:12 -0400

    Change over to 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
 
   // Folders to copy to output folder
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("_build/css");
 };
 
 function extractExcerpt(article) {
---
+++ _build/About.md
@@ -0,0 +1,11 @@
+--- 
+layout: base-layout.njk 
+--- 
+# This is my blog
+
+Welcome to my blog. There may be content eventually. Maybe not.
+  
+
+
+
+ 
